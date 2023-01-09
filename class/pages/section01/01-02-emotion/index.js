ade6fa1cbf3b1cc17764cef0e5eaafd681add9e4
kkasol <kkasol92@gmail.com>
2023-01-09 18:38:28 +0900

    프로젝트 저장하기

---
+++ class/pages/section01/01-02-emotion/index.js
@@ -0,0 +1,11 @@
+import { myInfo } from "../../../styles/01-02-emotion";
+
+export default function EmotionPage() {
+  //여기는 자바스크립트 쓰는 곳
+  return (
+    <div>
+      <myInfo>이메일: </myInfo>
+      <myInput type="text" />
+    </div>
+  );
+}
---
+++ class/styles/01-02-emotion.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const myInfo = styled.span`
+  color: red;
+`;
+
+export const myInput = styled.input``;
